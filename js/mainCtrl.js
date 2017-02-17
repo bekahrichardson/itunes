c12adbe75d919c2a8dad4273fb3fe9eb4354bbcd
Rebekah Richardson <bekahrichardson0422@gmail.com>
2017-02-17 11:42:34 -0700

    rest of project before I deferred

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,7 +1,7 @@
 angular.module('itunes').controller('mainCtrl', function($scope, itunesService){
   //This is setting up the default behavior of our ng-grid. The important thing to note is the 'data' property. The value is 'songData'. That means ng-grid is looking for songData on $scope and is putting whatever songData is into the grid.
   //This means when you make your iTunes request, you'll need to get back the information, parse it accordingly, then set it to songData on the scope -> $scope.songData = ...
-  $scope.gridOptions = { 
+  $scope.gridOptions = function() {
       data: 'songData',
       height: '110px',
       sortInfo: {fields: ['Song', 'Artist', 'Collection', 'Type'], directions: ['asc']},
@@ -18,16 +18,18 @@ angular.module('itunes').controller('mainCtrl', function($scope, itunesService){
 
   //Our controller is what's going to connect our 'heavy lifting' itunesService with our view (index.html) so our user can see the results they get back from itunes.
 
-  //First inject itunesService into your controller.
+  //First inject itunesService into your controller. (this is done already???)
 
 
   //Now write a function that will call the method on the itunesService that is responsible for getting the data from iTunes, whenever the user clicks the submit button
   //*remember, that method should be expecting an artist name. The artist name is coming from the input box on index.html, head over there and check if that input box is tied to any specific model we could use.
-  //Also note that that method should be retuning a promise, so you could use .then in this function. 
-    
-    //Code here
-  
+  //Also note that that method should be retuning a promise, so you could use .then in this function.
 
+    //Code here
+    function itunes(){
+  
+}
+//  ng-model="artist"
 
 
   //Check that the above method is working by entering a name into the input field on your web app, and then console.log the result.
@@ -35,10 +37,10 @@ angular.module('itunes').controller('mainCtrl', function($scope, itunesService){
 
 
 
-  //If everything worked you should see a huge array of objects inside your console. That's great! But unfortunately that's not what ng-grid is expecting. What you need to do now is sort the data you got back. This sounds like a great job for a service! Head back to your itunesService and complete the last instructions there. 
-  
+  //If everything worked you should see a huge array of objects inside your console. That's great! But unfortunately that's not what ng-grid is expecting. What you need to do now is sort the data you got back. This sounds like a great job for a service! Head back to your itunesService and complete the last instructions there.
 
-    
+
+
 
 
 
@@ -49,5 +51,33 @@ angular.module('itunes').controller('mainCtrl', function($scope, itunesService){
 });
 
 
+//angular.module('userProfiles').controller('MainController', function($scope, mainService) {
+//  $scope.getUsers = function() {
+//    mainService.getUsers().then(function(dataFromService) {
+//      $scope.users = dataFromService;
+//    });
+//  }
+//
+//  $scope.getUsers();
+//
+//});
 
 
+// angular.module('userProfiles').service('mainService', function($http, $q) {
+//
+//  this.getUsers = function() {
+//    var deferred = $q.defer();
+//    $http({
+//        method: 'GET',
+//        url: 'https://reqres.in/api/users?page=1'
+//    }).then(function(response) {
+//      console.log("this is a response", response)
+//      var parsedResponse = response.data.data
+//      for(var i = 0; i < parsedResponse.length; i++) {
+//        parsedResponse[i].first_name = 'Ralf'
+//      }
+//      deferred.resolve(parsedResponse)
+//    })
+//    return deferred.promise;
+//  }
+// });
